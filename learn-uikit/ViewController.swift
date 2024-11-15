a87103ba3b9fadb839a19a9d3cfa3b13facd3fe1
DHoangThanh1306 <dhoangthanh1306@gmail.com>
2024-11-15 17:02:15 +0700

    Add: overview Swift

--- learn-uikit/ViewController.swift
+++ learn-uikit/ViewController.swift
@@ -16,6 +16,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("view did load")
         
+        // how to create view by code
+        let frame = CGRect(x: 50, y: 50, width: 100, height: 100)
+        let userAvatar = UIImageView(image: UIImage(named: "LOGO"))
+        userAvatar.frame = frame
+        userAvatar.contentMode = .scaleToFill
+        view.addSubview(userAvatar)
+        
+        // how to listener event by code
+        let inputText = UILabel(frame: CGRect(x: 50, y: 200, width: 100, height: 25))
+        inputText.text = "DHT"
+        inputText.backgroundColor = .lightGray
+        inputText.textAlignment = .center
+        inputText.textColor = .blue
+        view.addSubview(inputText)
+
+        let button = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 25))
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(tap), for: .touchDragInside)
+        view.addSubview(button)
+        
+    }
+    
+    @objc func tap() {
+        print("tap tap")
     }
     
     @IBAction func buttonClicked(_ sender: UIButton) {
